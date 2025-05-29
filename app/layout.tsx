085238001da5052257ac82ca7c9c04c74550e7c8
Aymeric Dieu de Bellefontaine-Roy <aymeric.bellefontaine@gmail.com>
2025-05-29 22:47:36 +0200

    Initial commit for TypeFlick site (based on saas-starter)

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import './globals.css';
+import type { Metadata, Viewport } from 'next';
+import { Manrope } from 'next/font/google';
+import { getUser, getTeamForUser } from '@/lib/db/queries';
+import { SWRConfig } from 'swr';
+
+export const metadata: Metadata = {
+  title: 'TypeFlick',
+  description: 'Generate your type beat video and seo easy and fast'
+};
+
+export const viewport: Viewport = {
+  maximumScale: 1
+};
+
+const manrope = Manrope({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className = {`dark`}
+      // className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
+    >
+      <body className="min-h-[100dvh] bg-white text-black dark:bg-gray-950 dark:text-white">
+        <SWRConfig
+          value={{
+            fallback: {
+              // We do NOT await here
+              // Only components that read this data will suspend
+              '/api/user': getUser(),
+              '/api/team': getTeamForUser()
+            }
+          }}
+        >
+          {children}
+        </SWRConfig>
+      </body>
+    </html>
+  );
+}
